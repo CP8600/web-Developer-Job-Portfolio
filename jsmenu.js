650a44a913d6dc84e38217c2ecb9a0da1119c7e3
Christopher <74881159+CP8600@users.noreply.github.com>
2021-04-11 17:21:07 -0500

    Add files via upload

---
+++ jsmenu.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let menuSlide = () => {
+
+    /*    opens and closes menu */
+    let menu = document.getElementById("menuClose")
+    menu.classList.toggle("menuOpen")
+    menu.style.display = 'block'
+    menu.style.width = '250px'
+}
